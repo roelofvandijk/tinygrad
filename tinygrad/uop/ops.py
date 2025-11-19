897735b98442f0738a72b8326a9a3ca77ae9dc52
Roelof van Dijk <3604013+roelofvandijk@users.noreply.github.com>
2025-11-19 20:38:45 +0100

    faster limit check

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -1181,7 +1181,10 @@ class RewriteContext:
     on_stack = {root}  # all UOps either on the stack or in self.replace, i.e. dont have to be placed again
     REWRITE_STACK_LIMIT = getenv("REWRITE_STACK_LIMIT", 250000)
     while stack:
-      if len(stack) > REWRITE_STACK_LIMIT: raise RuntimeError("infinite loop in graph_rewrite (stack too big)")
+      try:
+        _ = stack[REWRITE_STACK_LIMIT+1]
+        raise RuntimeError("infinite loop in graph_rewrite (stack too big)")
+      except IndexError: pass
       n, stage, new_n = stack.pop()
       if n in self.replace: continue  # skip any nodes we have seen
       if stage == 0:
