86a114161dc51c4552245dd4e9836d4b5af06a5f
Roelof van Dijk <3604013+roelofvandijk@users.noreply.github.com>
2025-11-28 14:52:12 +0100

    last

--- tinygrad/renderer/wgsl.py
+++ tinygrad/renderer/wgsl.py
@@ -35,11 +35,6 @@ def is_packed(dt:DType, odt:DType|None = None) -> bool:
 wgsl_matcher = PatternMatcher([
   (UPat((Ops.CMPLT, Ops.XOR), src=(UPat(name="a", dtype=dtypes.bool), UPat.var("b")), name="c"),
    lambda a,b,c: a.cast(dtypes.int).alu(c.op, b.cast(dtypes.int)).cast(dtypes.bool)),
-  # webgpu doesn't support 64-bit ints - convert all 64-bit ops to 32-bit
-  (UPat((*GroupOp.Elementwise, Ops.LOAD, Ops.CONST, Ops.VCONST, Ops.INDEX, Ops.GEP, Ops.VECTORIZE),
-        dtype=(dtypes.uint64, dtypes.int64), name="x"),
-   lambda x: x.replace(dtype=dtypes.uint32 if x.dtype == dtypes.uint64 else dtypes.int32,
-                       src=tuple(s.cast(dtypes.uint32 if x.dtype == dtypes.uint64 else dtypes.int32) for s in x.src))),
   # TODO: load alt value doesnt have to be a const
   (UPat.load(UPat.var("b"), UPat.cvar("c"), allow_any_len=True, name="l"),
    lambda l,b,c: packed_load(l,b,l.dtype,c.cast(dtypes.uint32)) if is_packed(l.dtype, b.dtype) else None),
@@ -51,6 +46,10 @@ wgsl_matcher = PatternMatcher([
    lambda a,b: (a.bitcast(dtypes.uint32)<<b.cast(dtypes.uint32)).cast(
      a.dtype if a.dtype not in (dtypes.uint64, dtypes.int64) else dtypes.uint32) if b.dtype!=dtypes.uint32 else None),
   (UPat.var("x") >> UPat.var("y"), lambda x,y: UOp(Ops.SHR, x.dtype, (x,y.cast(dtypes.uint))) if y.dtype != dtypes.uint else None),
+  # webgpu doesn't support 64-bit ints - convert all 64-bit ops to 32-bit (LAST to avoid recursive matching)
+  (UPat(GroupOp.All, dtype=(dtypes.uint64, dtypes.int64), name="x"),
+   lambda x: x.replace(dtype=dtypes.uint32 if x.dtype == dtypes.uint64 else dtypes.int32,
+                       src=tuple(s.cast(dtypes.uint32 if x.dtype == dtypes.uint64 else dtypes.int32) for s in x.src))),
   ]) + extra_pm
 
 class WGSLRenderer(CStyleLanguage):
