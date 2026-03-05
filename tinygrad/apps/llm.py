ba15d6e2bd49828659afe9cf59cf8f70e76816c8
Roelof van Dijk <3604013+roelofvandijk@users.noreply.github.com>
2026-03-05 10:28:23 +0100

    works

--- tinygrad/apps/llm.py
+++ tinygrad/apps/llm.py
@@ -230,10 +230,12 @@ class GatedDeltaNetBlock:
     self.ssm_state = Tensor.zeros(B, self.num_v_heads, self.head_v_dim, self.head_v_dim, dtype="float32", device=x.device).contiguous().realize()
 
   def _gdn_attention(self, x:Tensor) -> Tensor:
+    # TODO: batched prefill
+    # TODO: function support?
     if (T:=x.shape[1].val if isinstance(x.shape[1], UOp) else x.shape[1]) == 1: return self._gdn_attention_step(x)
     out = Tensor.empty(*x.shape, dtype=x.dtype, device=x.device).contiguous()
     for t in range(T):
-      assigned = out.uop.after(out[:, t:t+1, :].uop.assign(self._gdn_attention_step(x[:, t:t+1, :]).contiguous().uop))
+      assigned = out.uop.after(out[:, t:t+1, :].uop.assign(self._gdn_attention_step(x[:, t:t+1, :]).contiguous().realize().uop))
       out = Tensor(assigned, device=assigned.device)
     return out
 
